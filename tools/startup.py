0d8428f76828dbb23bb5f7b1934521380ad03ba7
cmmalone <cmmalone.158@gmail.com>
2014-09-17 21:15:33 -0700

    svm starter code going in

--- tools/startup.py
+++ tools/startup.py
@@ -26,7 +26,7 @@ print "Enron dataset should be last item on the list, along with its current siz
 print "download will complete at about 423 MB"
 import urllib
 url = "https://www.cs.cmu.edu/~./enron/enron_mail_20110402.tgz"
-#urllib.urlretrieve(url, filename="../enron_mail_20110402.tgz") 
+urllib.urlretrieve(url, filename="../enron_mail_20110402.tgz") 
 print "download complete!"
 
 
