15a95dd44b80a10f222b2341331d7cf80e4e2538
cmmalone <cmmalone.158@gmail.com>
2014-09-17 22:05:28 -0700

    feature_format going in

---
+++ tools/feature_format.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+
+""" 
+    A general tool for converting data from the
+    dictionary format to an (n x k) python list that's 
+    ready for training an sklearn algorithm
+
+    n--no. of key-value pairs in dictonary
+    k--no. of features being extracted
+
+    dictionary keys are names of persons in dataset
+    dictionary values are dictionaries, where each
+        key-value pair in the dict is the name
+        of a feature, and its value for that person
+
+    In addition to converting a dictionary to a numpy 
+    array, you may want to separate the labels from the
+    features--this is what targetFeatureSplit is for
+
+    so, if you want to have the poi label as the target,
+    and the features you want to use are the person's
+    salary and bonus, here's what you would do:
+
+    feature_list = ["poi", "salary", "bonus"] 
+    data_array = featureFormat( data_dictionary, feature_list )
+    label, features = targetFeatureSplit(data_array)
+
+    the line above (targetFeatureSplit) assumes that the
+    label is the _first_ item in feature_list--very important
+    that poi is listed first!
+"""
+
+
+import numpy as np
+
+def featureFormat( dictionary, features, remove_NaN=True, remove_all_zeroes=True, remove_any_zeroes=False ):
+    """ convert dictionary to numpy array of features
+        remove_NaN=True will convert "NaN" string to 0.0
+        remove_all_zeroes=True will omit any data points for which
+            all the features you seek are 0.0
+        remove_any_zeroes=True will omit any data points for which
+            any of the features you seek are 0.0
+    """
+
+
+    return_list = []
+
+    for key in dictionary.keys():
+        tmp_list = []
+        append = False
+        for feature in features:
+            try:
+                dictionary[key][feature]
+            except KeyError:
+                print "error: key ", feature, " not present"
+                return
+            value = dictionary[key][feature]
+            if value=="NaN" and remove_NaN:
+                value = 0
+            tmp_list.append( float(value) )
+
+        ### if all features are zero and you want to remove
+        ### data points that are all zero, do that here
+        if remove_all_zeroes:
+            all_zeroes = True
+            for item in tmp_list:
+                if item != 0 and item != "NaN":
+                    append = True
+
+        ### if any features for a given data point are zero
+        ### and you want to remove data points with any zeroes,
+        ### handle that here
+        if remove_any_zeroes:
+            any_zeroes = False
+            if 0 in tmp_list or "NaN" in tmp_list:
+                append = False
+        if append:
+            return_list.append( np.array(tmp_list) )
+
+
+    return np.array(return_list)
+
+
+def targetFeatureSplit( data ):
+    """ 
+        given a numpy array like the one returned from
+        featureFormat, separate out the first feature
+        and put it into its own list (this should be the 
+        quantity you want to predict)
+
+        return targets and features as separate lists
+
+        (sklearn can generally handle both lists and numpy arrays as 
+        input formats when training/predicting)
+    """
+
+    target = []
+    features = []
+    for item in data:
+        target.append( item[0] )
+        features.append( item[1:] )
+
+    return target, features
+
+
+
+
