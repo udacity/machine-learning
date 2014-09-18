82958a765c7632c5fa39ec6beb5b3ab8fa96892b
cmmalone <cmmalone.158@gmail.com>
2014-09-18 10:26:05 -0700

    outliers starter code going in

---
+++ outliers/outlier_cleaner.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+
+def outlierCleaner(predictions, ages, net_worths):
+    """
+        clean away the 10% of points that have the largest
+        residual errors (different between the prediction
+        and the actual net worth)
+
+        return a list of tuples named cleaned_data where 
+        each tuple is of the form (age, net_worth, error)
+    """
+    
+    cleaned_data = []
+
+    ### your code goes here
+
+    
+    return cleaned_data
+
