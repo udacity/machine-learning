f3890592b78f7d9636cf9a061df56335f2636e80
cmmalone <cmmalone.158@gmail.com>
2014-09-18 13:20:20 -0700

    evaluation and validation added:

---
+++ evaluation/evaluate_poi_identifier.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+
+"""
+    starter code for the evaluation mini-project
+    start by copying your trained/tested POI identifier from
+    that you built in the validation mini-project
+
+    the second step toward building your POI identifier!
+
+    start by loading/formatting the data
+
+"""
+
+import pickle
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+### add more features to features_list!
+features_list = ["poi", "salary"]
+
+data = featureFormat(data_dict, features_list)
+labels, features = targetFeatureSplit(data)
+
+
+
+### your code goes here 
+
+
---
+++ validation/validate_poi.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+
+"""
+    starter code for the validation mini-project
+    the first step toward building your POI identifier!
+
+    start by loading/formatting the data
+
+    after that, it's not our code anymore--it's yours!
+"""
+
+import pickle
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+### add more features to features_list!
+features_list = ["poi", "salary"]
+
+data = featureFormat(data_dict, features_list)
+labels, features = targetFeatureSplit(data)
+
+
+
+### it's all yours from here forward!  
+
+
