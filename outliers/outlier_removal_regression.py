ba8d0e2c1a3963061d44ba5292ee4753c7b3e9a9
cmmalone <cmmalone.158@gmail.com>
2014-10-09 22:54:50 -0700

    adding train/test split to outlier regression

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -20,7 +20,8 @@ net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "r") )
 ### and n_columns is the number of features
 ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
 net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
-
+from sklearn.cross_validation import train_test_split
+ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1, random_state=42)
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
