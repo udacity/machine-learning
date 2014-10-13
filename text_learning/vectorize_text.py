7fa8c3182214f24a0a68cd9f344765e96baf08db
cmmalone <cmmalone.158@gmail.com>
2014-10-12 17:18:09 -0700

    temp_counter to only run over 200 emails when developing text processing algorithm

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -27,23 +27,34 @@ from_chris = open("from_chris.txt", "r")
 from_data = []
 word_data = []
 
+### temp_counter is a way to speed up the development--there are 
+### thousands of emails from Sara and Chris, so running over all of them
+### can take a long time
+### temp_counter helps you only look at the first 200 emails in the list
+temp_counter = 0
+
+
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
-    for path in from_person:
-        path = "../"+path[:-1]
-        print path
-        email = open(path, "r")
+    temp_counter += 1
+    ### only look at first 200 emails when developing
+    ### once everything is working, remove this line to run over full dataset
+    if temp_counter<200:
+        for path in from_person:
+            path = "../"+path[:-1]
+            print path
+            email = open(path, "r")
 
-        ### use parseOutText to extract the text from the opened email
+            ### use parseOutText to extract the text from the opened email
 
-        ### use str.replace() to remove any instances of the words
-        ### ["sara", "shackleton", "chris", "germani"] 
+            ### use str.replace() to remove any instances of the words
+            ### ["sara", "shackleton", "chris", "germani"] 
 
-        ### append the text to word_data
+            ### append the text to word_data
 
-        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
 
 
-    email.close()
+        email.close()
 
 print "emails processed"
 from_sara.close()
