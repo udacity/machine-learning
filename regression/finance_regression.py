43a618c97ea3c536242e319bf7078ce127aa3ae9
Sheng Kung Yi <shengkung@udacity.com>
2015-02-09 10:29:04 -0800

    minor comment cleanup

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -23,7 +23,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)#, "long_term_incentive"], remove_any_zeroes=True )
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
