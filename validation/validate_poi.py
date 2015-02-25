b452e9a42d507c000bf6d3068af425d9c0eda8fd
Sheng Kung Yi <shengkung@udacity.com>
2015-02-25 11:46:36 -0800

    Improve instructions for Lesson 13 mini-project.

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -17,7 +17,9 @@ from feature_format import featureFormat, targetFeatureSplit
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
-### add more features to features_list!
+### first element is our labels, any added elements are predictor
+### features. Keep this the same for the mini-project, but you'll
+### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
