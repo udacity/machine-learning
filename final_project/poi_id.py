7ea683725a7069757f61150ac1561fc5e2540225
Sheng Kung Yi <shengkungmichaelyi@Shengs-MacBook-Air.local>
2015-08-20 16:02:45 -0700

    updated instructions for final project
    Update poi_id.py with more instructions for tasks to be performed.

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -5,7 +5,7 @@ import pickle
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
-from tester import test_classifier, dump_classifier_and_data
+from tester import dump_classifier_and_data
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -30,18 +30,25 @@ labels, features = targetFeatureSplit(data)
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
+# Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()    # Provided to give you a starting point. Try a varity of classifiers.
+clf = GaussianNB()
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script.
-### Because of the small size of the dataset, the script uses stratified
-### shuffle split cross validation. For more info: 
+### using our testing script. Check the tester.py script in the final project
+### folder for details on the evaluation method, especially the test_classifier
+### function. Because of the small size of the dataset, the script uses
+### stratified shuffle split cross validation. For more info: 
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-test_classifier(clf, my_dataset, features_list)
+# Example starting point. Try investigating other evaluation techniques!
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
 
-### Dump your classifier, dataset, and features_list so 
-### anyone can run/check your results.
+### Task 6: Dump your classifier, dataset, and features_list so anyone can
+### check your results. You do not need to change anything below, but make sure
+### that the version of poi_id.py that you submit can be run on its own and
+### generates the necessary .pkl files for validating your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
