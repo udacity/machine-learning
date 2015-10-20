81c22055437b03de2a18951e5eccc875ef2c39ff
Sheng Kung Yi <shengkung@udacity.com>
2015-10-19 17:54:03 -0700

    revert to base state
    previous commit accidentally included solution, reverting to original state

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -62,26 +62,15 @@ for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
-
-
-from sklearn.cluster import KMeans
-features_list = ["poi", feature_1, feature_2]
-data2 = featureFormat(data_dict, features_list )
-poi, finance_features = targetFeatureSplit( data2 )
-clf = KMeans(n_clusters=2)
-pred = clf.fit_predict( finance_features )
-Draw(pred, finance_features, poi, name="clusters_before_scaling.pdf", f1_name=feature_1, f2_name=feature_2)
-
-
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+
+
+
+### rename the "name" parameter when you change the number of features
+### so that the figure gets saved to a different file
 try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
-
-
-
-
-
