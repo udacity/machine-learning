b8103a7c767a0addb8a457441e1baf34f84d0a69
drsimonj <drsimonjackson@gmail.com>
2016-12-23 14:24:41 +1100

    Enable random smartcab actions

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -107,7 +107,12 @@ class LearningAgent(Agent):
         # When not learning, choose a random action
         # When learning, choose a random action with 'epsilon' probability
         #   Otherwise, choose an action with the highest Q-value for the current state
- 
+        if not self.learning:
+            action = random.choice(self.valid_actions)
+        else:
+            action = None
+
+
         return action
 
 
