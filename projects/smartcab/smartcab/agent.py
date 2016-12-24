6256aa9464f9be4e9d378753570afc83698f88f8
drsimonj <drsimonjackson@gmail.com>
2016-12-25 09:09:42 +1100

    Turn smartcab log_metrics on again

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -208,7 +208,7 @@ def run(file_suffix=''):
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay = 0.01, log_metrics = False, optimized = True, display = False,
+    sim = Simulator(env, update_delay = 0.01, log_metrics = True, optimized = True, display = False,
                     file_suffix=file_suffix)
 
     ##############
