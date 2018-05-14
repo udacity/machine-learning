99c501ec5adc8d0511435af61edb8d76fe79ba8d
jo4x962k7JL <dna2inics@gmail.com>
2018-05-13 19:10:03 -0700

    Project 1: Boston Housing 2nd submission
    new file:   boston_housing/Grid_Search_vs_Random_Search.png
    modified:   boston_housing/boston_housing.ipynb
    modified:   boston_housing/report.html
    modified:   boston_housing/visuals.py

--- projects/boston_housing/visuals.py
+++ projects/boston_housing/visuals.py
@@ -18,7 +18,7 @@ from sklearn.cross_validation import ShuffleSplit, train_test_split
 def ModelLearning(X, y):
     """ Calculates the performance of several models with varying sizes of training data.
         The learning and testing scores for each model are then plotted. """
-    
+
     # Create 10 cross-validation sets for training and testing
     cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
 
@@ -30,21 +30,21 @@ def ModelLearning(X, y):
 
     # Create three different models based on max_depth
     for k, depth in enumerate([1,3,6,10]):
-        
+
         # Create a Decision tree regressor at max_depth = depth
         regressor = DecisionTreeRegressor(max_depth = depth)
 
         # Calculate the training and testing scores
         sizes, train_scores, test_scores = curves.learning_curve(regressor, X, y, \
             cv = cv, train_sizes = train_sizes, scoring = 'r2')
-        
+
         # Find the mean and standard deviation for smoothing
         train_std = np.std(train_scores, axis = 1)
         train_mean = np.mean(train_scores, axis = 1)
         test_std = np.std(test_scores, axis = 1)
         test_mean = np.mean(test_scores, axis = 1)
 
-        # Subplot the learning curve 
+        # Subplot the learning curve
         ax = fig.add_subplot(2, 2, k+1)
         ax.plot(sizes, train_mean, 'o-', color = 'r', label = 'Training Score')
         ax.plot(sizes, test_mean, 'o-', color = 'g', label = 'Testing Score')
@@ -52,14 +52,14 @@ def ModelLearning(X, y):
             train_mean + train_std, alpha = 0.15, color = 'r')
         ax.fill_between(sizes, test_mean - test_std, \
             test_mean + test_std, alpha = 0.15, color = 'g')
-        
+
         # Labels
         ax.set_title('max_depth = %s'%(depth))
         ax.set_xlabel('Number of Training Points')
         ax.set_ylabel('Score')
         ax.set_xlim([0, X.shape[0]*0.8])
         ax.set_ylim([-0.05, 1.05])
-    
+
     # Visual aesthetics
     ax.legend(bbox_to_anchor=(1.05, 2.05), loc='lower left', borderaxespad = 0.)
     fig.suptitle('Decision Tree Regressor Learning Performances', fontsize = 16, y = 1.03)
@@ -70,7 +70,7 @@ def ModelLearning(X, y):
 def ModelComplexity(X, y):
     """ Calculates the performance of the model as model complexity increases.
         The learning and testing errors rates are then plotted. """
-    
+
     # Create 10 cross-validation sets for training and testing
     cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
 
@@ -96,7 +96,7 @@ def ModelComplexity(X, y):
         train_mean + train_std, alpha = 0.15, color = 'r')
     pl.fill_between(max_depth, test_mean - test_std, \
         test_mean + test_std, alpha = 0.15, color = 'g')
-    
+
     # Visual aesthetics
     pl.legend(loc = 'lower right')
     pl.xlabel('Maximum Depth')
@@ -115,16 +115,16 @@ def PredictTrials(X, y, fitter, data):
         # Split the data
         X_train, X_test, y_train, y_test = train_test_split(X, y, \
             test_size = 0.2, random_state = k)
-        
+
         # Fit the data
         reg = fitter(X_train, y_train)
-        
+
         # Make a prediction
         pred = reg.predict([data[0]])[0]
         prices.append(pred)
-        
+
         # Result
-        print "Trial {}: ${:,.2f}".format(k+1, pred)
+        print("Trial {}: ${:,.2f}".format(k+1, pred))
 
     # Display price range
-    print "\nRange in prices: ${:,.2f}".format(max(prices) - min(prices))
+    print("\nRange in prices: ${:,.2f}".format(max(prices) - min(prices)))
